504bb8853e43622af82d0957d8722acf98388170
Shivam Soni <vermashivam217@gmail.com>
2021-03-01 23:12:28 +0530

    auth system

---
+++ routes/web.js
@@ -0,0 +1,44 @@
+const homeController = require('../app/http/controllers/homeController')
+const authController = require('../app/http/controllers/authController')
+const cartController = require('../app/http/controllers/customers/cartController')
+const orderController = require('../app/http/controllers/customers/orderController')
+//const AdminOrderController = require('../app/http/controllers/admin/orderController')
+
+// Middlewares
+const guest = require('../app/http/middleware/guest')
+const auth = require('../app/http/middleware/auth')
+//const admin = require('../app/http/middleware/admin')
+
+
+function initRoutes(app) {
+    
+    app.get('/', homeController().index)
+    
+    app.get('/login', guest, authController().login)
+
+    app.post('/login', authController().postLogin)
+    
+    app.get('/register', guest, authController().register)
+
+    app.post('/register', authController().postRegister)
+
+    app.post('/logout', authController().logout)
+    
+    app.get('/cart', cartController().index)
+
+    app.post('/update-cart', cartController().update)
+
+    app.post('/orders', orderController().store)
+
+    //Customer routes
+    app.post('/orders', auth, orderController().store)
+    app.get('/customer/orders', auth, orderController().index)
+
+    // Admin routes
+    //app.get('/admin/orders', auth, AdminOrderController().index)
+
+    
+}
+
+
+module.exports = initRoutes
